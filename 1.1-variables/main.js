57463f0e21902c275e8ecbc8ffe2bd2d4fc7ed40
Iva Kochneva <inkybasik@gmail.com>
2020-01-25 18:14:53 +0300

    Add homework 1.1

---
+++ 1.1-variables/main.js
@@ -0,0 +1,40 @@
+/* Задача № 1
+https://github.com/netology-code/bjs-homeworks/tree/master/1.1-variables#%D0%B7%D0%B0%D0%B4%D0%B0%D1%87%D0%B0--1
+*/
+
+let algebra = 0;
+let geography = 0;
+let physics = 0;
+
+algebra = 5;
+geography = 4;
+physics = 4;
+
+let averageRating = 0;
+
+averageRating = (algebra + geography + physics) / 3;
+console.log(`Средняя оценка за алгебру, географию и физику: ${averageRating}`);
+
+/* Задача № 2
+https://github.com/netology-code/bjs-homeworks/tree/master/1.1-variables#%D0%B7%D0%B0%D0%B4%D0%B0%D1%87%D0%B0--2 */
+
+let myName = 'Мария Степановна';
+let message = `Привет, мир! Меня зовут ${myName}.`
+
+console.log(message);
+
+/* Задача № 3
+https://github.com/netology-code/bjs-homeworks/tree/master/1.1-variables#%D0%B7%D0%B0%D0%B4%D0%B0%D1%87%D0%B0--3
+*/
+
+let x = 0;
+let y = 0;
+let z = 0;
+
+x = 2;
+y = 22;
+z = 0;
+
+let result =  x * y + 5 * z + x - 1;
+
+console.log(`x * y + 5 * z + x - 1 = ${result}`);
